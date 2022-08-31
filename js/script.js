e8d5ddef82734b5656d6ef67d99def58adfae86f
Nathan Schreiber <nathanschreiber76@gmail.com>
2022-08-31 17:47:32 -0400

    Add ability to input custom  grid size
    Added custom grid size functionality and moved drawing/reset functions
    into new event listener to work with custom sizes.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Grabs html elements to use in JS
 const container = document.querySelector('#container');
 const resetButton = document.querySelector('.reset');
+const sizeButton = document.querySelector('.grid-size-button');
 
 // Function that makes rows and columns based on input number
 function makeRows(rows, cols) {
@@ -8,25 +9,31 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-columns', cols);
     for (i = 0; i < (rows * cols); i++) {
       let cell = document.createElement("div");
-      cell.classList.add('test');
       container.appendChild(cell).className = "grid-item";
     };
   };
 
-  makeRows (16, 16);
-
-const gridItem = document.querySelectorAll('.grid-item');
-
-// Makes grid colored when hovered over with mouse
-for (let i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseover', () => {
-        gridItem[i].classList.add('hover-styles');
-    });
+// Prompt for choosing grid dimensions
+function gridSize() {
+    let w = prompt("Enter Width", 16);
+    let h = w;
+    makeRows(h, w);
 };
 
-// Resets grid to original colors
-resetButton.addEventListener('click', () => {
+// Gets dimensions after size button is clicked, then enables drawing.
+sizeButton.addEventListener('click', () => {
+    gridSize();
+    const gridItem = document.querySelectorAll('.grid-item');
+    // Makes grid colored when hovered over with mouse
     for (let i = 0; i < gridItem.length; i++) {
-        gridItem[i].classList.remove('hover-styles');
+        gridItem[i].addEventListener('mouseover', () => {
+            gridItem[i].classList.add('hover-styles');
+        });
     };
+    // Resets grid to original colors
+    resetButton.addEventListener('click', () => {
+        for (let i = 0; i < gridItem.length; i++) {
+            gridItem[i].classList.remove('hover-styles');
+        };
+    });
 });
